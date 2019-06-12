8d74c2a7a6d8f27e22e4d0faa01be86e4a51f025
dstaka <da1sc0sg@gmail.com>
2019-06-13 01:15:11 +0800

    remove old file

--- predict_dangerdrive.py
+++
@@ -1,8 +0,0 @@
-import time
-import logging
-import logging.config
-
-
-# Set logger
-logging.config.fileConfig('./config/logging.conf', disable_existing_loggers=False)
-logger = logging.getLogger('root')
