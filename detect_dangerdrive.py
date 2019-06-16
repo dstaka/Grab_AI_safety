9fc589b5ebea1a12ca762da26891bf6aa4ae170e
dstaka <da1sc0sg@gmail.com>
2019-06-17 00:12:25 +0800

    remove irrelevant metrics

--- detect_dangerdrive.py
+++ detect_dangerdrive.py
@@ -57,14 +57,6 @@ def score_dangerdrive(_df):
     # Calculate AUC
     roc_auc_score_tmp = roc_auc_score(y_test, predicted_prob)
     logger.info('Test AUC: ' + str(roc_auc_score_tmp))
-    # Calculate F1_score
-    y_test_val = y_test.get_values()
-    y_bin = [1. if y_test > 0.5 else 0. for y_cont in predicted_prob] # binaryzing output
-    f1_score_tmp = f1_score(y_test_val, y_bin)
-    logger.info('Test F1-score: ' + str(f1_score_tmp))
-    # Calculate neg_log_loss
-    log_loss_score_tmp = log_loss(y_test, predicted_prob)
-    logger.info('Test log_loss: ' + str(log_loss_score_tmp))
     # Calculate accuracy
     accuracy_score_tmp = accuracy_score(y_test_val, y_bin)
     logger.info('Test accuracy: ' + accuracy_score_tmp)
